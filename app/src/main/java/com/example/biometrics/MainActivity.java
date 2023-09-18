4bb1d90f8912c75d4cdd4cf6bdc24469e54b638a
Noam <NoamZecharia1808@gmail.com>
2023-09-18 11:58:17 +0300

    a

--- app/src/main/java/com/example/biometrics/MainActivity.java
+++ app/src/main/java/com/example/biometrics/MainActivity.java
@@ -1,14 +1,59 @@
 package com.example.biometrics;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricPrompt;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
+import androidx.core.content.ContextCompat;
 
+import android.content.res.AssetFileDescriptor;
+import android.hardware.biometrics.BiometricManager;
+import android.icu.text.ConstrainedFieldPosition;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.security.KeyStore;
+import java.util.Set;
+import java.util.concurrent.Executor;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button fingerPrint;
+    BiometricPrompt biometricPrompt;
+    BiometricPrompt.PromptInfo promptInfo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button fingerPrint = (Button)findViewById(R.id.fingerPrint);
+
+        fingerPrint.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Executor executor = ContextCompat.getMainExecutor(MainActivity.this);
+                biometricPrompt = new BiometricPrompt(MainActivity.this, executor, new BiometricPrompt.AuthenticationCallback() {
+                    @Override
+                    public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+                        super.onAuthenticationError(errorCode, errString);
+                    }
+
+                    @Override
+                    public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+                        super.onAuthenticationSucceeded(result);
+                        Toast.makeText(getApplicationContext(), "Login succeed ", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onAuthenticationFailed() {
+                        super.onAuthenticationFailed();
+                    }
+                });
+            }
+        });
+
     }
 }
