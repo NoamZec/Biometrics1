401e169a85260f502bd73ae4e7946b0c307a3475
Noam <NoamZecharia1808@gmail.com>
2023-09-22 10:59:15 +0300

    fingerpring & camera done

--- app/src/main/java/com/example/biometrics/MainActivity.java
+++ app/src/main/java/com/example/biometrics/MainActivity.java
@@ -5,15 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.biometric.BiometricPrompt;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.res.AssetFileDescriptor;
+import android.graphics.Bitmap;
 import android.hardware.biometrics.BiometricManager;
 import android.icu.text.ConstrainedFieldPosition;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.security.KeyStore;
@@ -22,30 +29,67 @@ import java.util.concurrent.Executor;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button fingerPrint;
+    private ImageView imageView;
+    private Button fingerPrint;
     private BiometricPrompt biometricPrompt;
     private BiometricPrompt.PromptInfo promptInfo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button fingerPrint = (Button)findViewById(R.id.fingerPrint);
+        fingerPrint = (Button)findViewById(R.id.fingerPrint);
+        imageView = findViewById(R.id.imageView);
 
-        fingerPrint.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                BiometricPrompt.PromptInfo promptInfo =
-                        new BiometricPrompt.PromptInfo.Builder()
-                                .setTitle("Biometric login for my app")
-                                .setSubtitle("Log in using your biometric credential")
-                                .setNegativeButtonText("Cancel")
-                                .build();
-            }
+        fingerPrint.setOnClickListener(v -> {
+            BiometricPrompt.PromptInfo promptInfo =
+                    new BiometricPrompt.PromptInfo.Builder()
+                            .setTitle("Biometric login for my app")
+                            .setSubtitle("Log in using your biometric credential")
+                            .setNegativeButtonText("Cancel")
+                            .build();
+            BiometricPrompt biometricPrompt1 = AfterFingerPrint();
+            biometricPrompt1.authenticate(promptInfo);
         });
 
     }
 
-    public BiometricPrompt AfterFingerPrint(){
+    private void openCamera() {
+        if(askForCameraPermission()) {
+           Intent camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+           startActivityForResult(camera, 1);
+        }
+    }
+
+    private boolean askForCameraPermission() {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{android.Manifest.permission.CAMERA}, 101);
+            return ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
+        }
+        return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1) {
+            Bitmap image = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(image);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 101) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                openCamera();
+            } else {
+                Toast.makeText(this, "Camera Permission is Required to Use Camera", Toast.LENGTH_SHORT).show();
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+    public BiometricPrompt AfterFingerPrint() {
         Executor executor = ContextCompat.getMainExecutor(MainActivity.this);
         biometricPrompt = new BiometricPrompt(MainActivity.this, executor, new BiometricPrompt.AuthenticationCallback() {
             @Override
@@ -57,6 +101,7 @@ public class MainActivity extends AppCompatActivity {
             public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
                 super.onAuthenticationSucceeded(result);
                 Toast.makeText(getApplicationContext(), "Login succeed ", Toast.LENGTH_SHORT).show();
+                openCamera();
             }
 
             @Override
@@ -67,4 +112,5 @@ public class MainActivity extends AppCompatActivity {
 
         return biometricPrompt;
     }
+
 }
